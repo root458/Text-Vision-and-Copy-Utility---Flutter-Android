c262b310e0baa9cdbd4d9e29f5b4949b57417dd6
root458 <kagame458@protonmail.com>
2022-01-24 00:25:27 +0300

    * Set up camera view * Set up the UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:vision_text/src/home_page.dart';
 
-void main() {
-  runApp(const MyApp());
+List<CameraDescription>? cameras;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  cameras = await availableCameras();
+  runApp(MyApp(cameras: cameras!,));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key, required this.cameras}) : super(key: key);
+  final List<CameraDescription> cameras;
 
   // This widget is the root of your application.
   @override
@@ -16,9 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: HomePage(cameras: cameras,),
     );
   }
 }
-
-
--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -1,12 +1,86 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.cameras}) : super(key: key);
+
+  final List<CameraDescription> cameras;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late CameraController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(widget.cameras[0], ResolutionPreset.max);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return const Scaffold(
+        body: Center(
+          child: SpinKitDoubleBounce(
+            color: Colors.blue,
+            size: 50.0,
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Colors.black87,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.flash_on),
+          onPressed: () => controller.setFlashMode(FlashMode.torch),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        title: const Text('Vision Text'),
+      ),
+      body: Column(
+        children: [
+          Flexible(flex: 8, child: CameraPreview(controller)),
+          Flexible(
+              child: Center(
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+              label: const Text(
+                'Analyze text',
+                style: TextStyle(color: Colors.black),
+              ),
+              style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all(Size(
+                      MediaQuery.of(context).size.width * 0.7,
+                      MediaQuery.of(context).size.height * 0.06)),
+                  backgroundColor: MaterialStateProperty.all(Colors.white)),
+            ),
+          ))
+        ],
+      ),
     );
   }
 }
